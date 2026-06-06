c6c182e771d8acce84181f64eb4ac4e4b10b48eb
imamhossenbu <imam.cse9.bu@gmail.com>
2026-06-06 16:46:01 +0600

    modify

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,31 +1,28 @@
-/* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import Redis from 'ioredis';
+import { Injectable } from '@nestjs/common';
+import { Redis } from '@upstash/redis';
 
 @Injectable()
-export class RedisService implements OnModuleInit, OnModuleDestroy {
-  private client!: Redis;
+export class RedisService {
+  private client: Redis;
 
-  onModuleInit() {
-    const redisUrl = process.env.REDIS_URL ?? 'redis://localhost:6379';
-    this.client = new Redis(redisUrl);
-  }
-
-  onModuleDestroy() {
-    this.client.disconnect();
+  constructor() {
+    this.client = new Redis({
+      url: process.env.UPSTASH_REDIS_REST_URL || '',
+      token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
+    });
   }
 
   async get(key: string): Promise<string | null> {
-    return this.client.get(key);
+    return await this.client.get(key);
   }
 
   async set(key: string, value: string, ttlSeconds?: number): Promise<void> {
     if (ttlSeconds) {
-      await this.client.set(key, value, 'EX', ttlSeconds);
+      await this.client.set(key, value, { ex: ttlSeconds });
     } else {
       await this.client.set(key, value);
     }
